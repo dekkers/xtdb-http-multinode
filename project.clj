1a89b7b1af881d4846685e0587b80aea2fc4fe0c
Jeroen Dekkers <jeroen@dekkers.ch>
2023-01-23 13:53:10 +0100

    Fix info in project.ctl

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject xtdb-http-multinode "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "XTDB http-server with multinode support"
+  :url "https://github.com/dekkers/xtdb-http-multinode"
+  :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [com.xtdb/xtdb-core "1.22.1"]
                  [com.xtdb/xtdb-rocksdb "1.22.1"]
